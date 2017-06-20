cc08b74ee02c448dba8c35d0d33b7bff556f2ae7
Brayan Espinoza <bjespinozah@gmail.com>
2017-06-19 21:45:39 -0500

    add DelphiProject class

---
+++ src/main/java/survey/model/DelphiProject.java
@@ -0,0 +1,63 @@
+package survey.model;
+
+import java.util.Date;
+
+public class DelphiProject {
+    private int id;
+    private String projectName;
+    private String themeName;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    public DelphiProject() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
