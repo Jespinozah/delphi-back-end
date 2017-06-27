ee972ccb50cfa6fba856fc7424568fbd47072d9c
jespinozah <bjespinozah@gmail.com>
2017-06-27 14:27:30 -0500

    add SurveyTemplate model

---
+++ src/main/java/survey/model/SurveyTemplate.java
@@ -0,0 +1,63 @@
+package survey.model;
+
+import java.util.Date;
+
+public class SurveyTemplate {
+    private int id;
+    private String name;
+    private String indicaciones;
+    private boolean status;
+    private Date startDate;
+    private Date endDate;
+
+    public SurveyTemplate() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIndicaciones() {
+        return indicaciones;
+    }
+
+    public void setIndicaciones(String indicaciones) {
+        this.indicaciones = indicaciones;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
